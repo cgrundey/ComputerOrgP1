45e05cd9a119789037345839cd8d17f638cc130d
= <=>
2017-10-23 13:04:42 -0400

    Finishing up error checking and using command line arguments

--- GrundeyAssembler.py
+++ GrundeyAssembler.py
@@ -1,11 +1,19 @@
 # Python Assemebler
 
 import sys
+import os
 
-# Keep track of what instruction to be able to jump later
+if len(sys.argv) != 3:
+    sys.exit('Wrong number of arguments')
+if not os.path.isfile(sys.argv[1]):
+    sys.exit('Input file does not exist')
+
+inFileName = sys.argv[1]
+outFileName = sys.argv[2]
+
+# Keep track of instruction to branch to later
 instructionCount = 0
 
-# all r-type opcodes are 0x0
 rTypeFuncts = {'add': '100000', 'addu': '100001', 'and': '100100', 'nor': '100111', 'or': '100101', 'slt': '101010',
                'sltu': '101011', 'sll': '000000', 'srl': '000010', 'sub': '100010', 'subu': '100011'}
 iTypeOps = {'addi': '001000', 'addiu': '001001', 'andi': '001100', 'beq': '000100', 'bne': '000101', 'lbu': '100100',
@@ -17,9 +25,12 @@ regs = {'$zero': '00000', '$at': '00001', '$v0': '00010', '$v1': '00011', '$a0':
         '$s4': '10100', '$s5': '10101', '$s6': '10110', '$s7': '10111', '$t8': '11000', '$t9': '11001', '$k0': '11010',
         '$k1': '11011', '$gp': '11100', '$sp': '11101', '$fp': '11110', '$ra': '11111'}
 
+# Branch labels and locations are stored in a dictionary
 labels = dict()
+# Final instructions recorded in hex are stored in a list
 hexInstructions = list()
 
+# TODO: use command line arguments
 # Parsing labels
 with open('test cases/test_case3.s', 'r') as assemblyFile:
     lineCount = 1
@@ -30,17 +41,27 @@ with open('test cases/test_case3.s', 'r') as assemblyFile:
             n += 1
         lineCount += 1
 
+linenum = 0  # for error output
+
+# TODO: use command line arguments
 # Parsing instructions
 with open('test cases/test_case3.s', 'r') as assemblyFile:
     for line in assemblyFile:
+        linenum += 1
         if ':' not in line:
             instructionCount += 1
             line = line[1:]
             instruction = line[:line.find("\t") + 1].rstrip("\t")
             line = line[line.find("\t") + 1:]
             operands = line.rstrip("\n").split(", ")
+
             # R-Type instructions
             if instruction in rTypeFuncts:
+                if len(operands) != 3:
+                    sys.exit('Incorrect format at line [' + linenum + ']')
+                if operands[0] not in regs or operands[1] not in regs:
+                    sys.exit('Invalid register given at line [' + linenum + ']');
+
                 opcode = '000000'
                 functCode = rTypeFuncts[instruction]
                 rd = regs[operands[0]]
@@ -49,22 +70,36 @@ with open('test cases/test_case3.s', 'r') as assemblyFile:
                     rt = regs[operands[1]]
                     rs = '00000'
                     shamt = str(bin(int(operands[2]))[2:].zfill(5))
-                # Standard R-Type instructions
+                # Other R-Type instructions
                 else:
+                    if operands[2] not in regs:
+                        sys.exit('Invalid register given at line [' + linenum + ']');
+
                     rs = regs[operands[1]]
                     rt = regs[operands[2]]
                     shamt = '00000'
                 result = opcode + rs + rt + rd + shamt + functCode
                 hexInstructions.append(hex(int(result, 2))[2:].zfill(8))
+
             # I-Type instructions
             elif instruction in iTypeOps:
                 opcode = iTypeOps[instruction]
                 # Load & Store Word
                 if instruction == 'lw' or instruction == 'sw':
+                    if len(operands) != 2:
+                        sys.exit('Incorrect format at line [' + linenum + ']')
+                    if operands[0] not in regs or operands[1][operands[1].find('(') + 1:operands[1].find(')')] not in regs:
+                        sys.exit('Incorrect format at line [' + linenum + ']')
+
                     rt = regs[operands[0]]
                     rs = regs[operands[1][operands[1].find('(') + 1:operands[1].find(')')]]
                     imm = operands[1][:operands[1].find('(')]
                 else:
+                    if len(operands) != 3:
+                        sys.exit('Incorrect format at line [' + linenum + ']')
+                    if operands[0] not in regs or operands[1] not in regs:
+                        sys.exit('Incorrect format at line [' + linenum + ']')
+
                     rt = regs[operands[0]]
                     rs = regs[operands[1]]
                     imm = operands[2]
@@ -74,17 +109,23 @@ with open('test cases/test_case3.s', 'r') as assemblyFile:
                             rt = regs[operands[1]]
                             imm = str(labels[imm] - instructionCount - 1)
                         else:
-                            sys.exit('Label does not exist: ' + imm)
+                            sys.exit('Label does not exist at line [' + linenum + ']')
 
                 if int(imm) < 0:
-                    imm = bin(int(imm) & 0xffff)[2:]  # convert to binary string
+                    imm = bin(int(imm) & 0xffff)[2:]
                 else:
-                    imm = bin(int(imm))[2:].zfill(16)  # convert to binary string
+                    imm = bin(int(imm))[2:].zfill(16)
                 result = opcode + rs + rt + imm
                 hexInstructions.append(hex(int(result, 2))[2:].zfill(8))
 
+            else:
+                sys.exit('Instruction not supported at line [' + linenum + ']')
 
+# TODO: use command line arguments
 # TODO: Write file out as hexadecimial / binary
-f = open("output.o", "w")
-for i in hexInstructions:
-    f.write(str(i) + '\n')
+# Output hex instructions to object file
+with open("output.obj", "wb") as f:
+    for i in hexInstructions:
+        # f.write(str(i) + '\n') # writes as string to object file
+        f.write(bytes(i))
+        f.write(b'\n')
